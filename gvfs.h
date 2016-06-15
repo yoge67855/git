0c292a526407d964a230aac86fa44759b74d87e3
Ben Peart <Ben.Peart@microsoft.com>
2016-06-15 14:59:16 +0000

    gvfs: ensure all filters and EOL conversions are blocked
    Ensure all filters and EOL conversions are blocked when running under
    GVFS so that our projected file sizes will match the actual file size
    when it is hydrated on the local machine.
    
    Signed-off-by: Ben Peart <Ben.Peart@microsoft.com>

--- gvfs.h
+++ gvfs.h
@@ -19,6 +19,7 @@
 #define GVFS_NO_DELETE_OUTSIDE_SPARSECHECKOUT       (1 << 3)
 #define GVFS_FETCH_SKIP_REACHABILITY_AND_UPLOADPACK (1 << 4)
 /*#define GVFS_LOWER_DEFAULT_SLOP                     (1 << 5)*/
+#define GVFS_BLOCK_FILTERS_AND_EOL_CONVERSIONS      (1 << 6)
 
 static inline int gvfs_config_is_set(int mask) {
 	return (core_gvfs & mask) == mask;
