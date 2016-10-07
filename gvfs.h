472b21b3e4df8ae52adbb29ca9397c73d010c776
Ben Peart <Ben.Peart@microsoft.com>
2016-10-07 14:17:57 +0000

    gvfs: modify default options for merge
    This sets a couple of the options for merge.
    
    1. turn on aggressive - this is to try to resolve a few more trivial
       merge cases.  It is documented in read-tree and is not something you
       can pass into merge itself.
    
    2. turn off detect_rename - this is to save git from having to go
       through the entire 3 trees to see if there were any renames that
       happened.
    
    For the work item repro that I have been using this drops the merge time
    from ~1 hour to ~5 minutes and the unmerged entries goes down from
    ~40,000 to 1.
    
    Signed-off-by: Ben Peart <Ben.Peart@microsoft.com>

--- gvfs.h
+++ gvfs.h
@@ -21,6 +21,7 @@
 /*#define GVFS_LOWER_DEFAULT_SLOP                     (1 << 5)*/
 #define GVFS_BLOCK_FILTERS_AND_EOL_CONVERSIONS      (1 << 6)
 #define GVFS_SKIP_MERGE_IN_CHECKOUT                 (1 << 7)
+#define GVFS_DEFAULT_MERGE_OPTIONS                  (1 << 8)
 
 static inline int gvfs_config_is_set(int mask) {
 	return (core_gvfs & mask) == mask;
