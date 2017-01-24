a0af267e90dd7fc2a30e2a589cb1ef9490e923ed
Kevin Willford <kewillf@microsoft.com>
2017-01-24 17:34:12 +0100

    gvfs: add the core.gvfs config setting
    This does not do anything yet. The next patches will add various values
    for that config setting that correspond to the various features
    offered/required by GVFS.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>

--- config.c
+++ config.c
@@ -16,6 +16,7 @@
 #include "string-list.h"
 #include "utf8.h"
 #include "dir.h"
+#include "gvfs.h"
 
 struct config_source {
 	struct config_source *prev;
@@ -1223,6 +1224,11 @@ static int git_default_core_config(const char *var, const char *value, void *cb)
 			object_creation_mode = OBJECT_CREATION_USES_HARDLINKS;
 		else
 			die(_("invalid mode for object creation: %s"), value);
+		return 0;
+	}
+
+	if (!strcmp(var, "core.gvfs")) {
+		gvfs_load_config_value(value);
 		return 0;
 	}
 
