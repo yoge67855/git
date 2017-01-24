824e4a1e8b539c314205a14d8b0d4504dc65adda
Kevin Willford <kewillf@microsoft.com>
2017-01-24 17:34:12 +0100

    gvfs: add the core.gvfs config setting
    This does not do anything yet. The next patches will add various values
    for that config setting that correspond to the various features
    offered/required by GVFS.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>

--- gvfs.h
+++ gvfs.h
@@ -1,9 +1,40 @@
 #ifndef GVFS_H
 #define GVFS_H
 
+#include "cache.h"
+#include "config.h"
+
 /*
  * This file is for the specific settings and methods
  * used for GVFS functionality
  */
+
+static inline int gvfs_config_is_set(int mask) {
+	return (core_gvfs & mask) == mask;
+}
+
+static inline int gvfs_config_is_set_any(void) {
+	return core_gvfs > 0;
+}
+
+static inline void gvfs_load_config_value(const char *value) {
+	int is_bool = 0;
+
+	if (value)
+		core_gvfs = git_config_bool_or_int("core.gvfs", value, &is_bool);
+	else
+		git_config_get_bool_or_int("core.gvfs", &is_bool, &core_gvfs);
+
+	/* Turn on all bits if a bool was set in the settings */
+	if (is_bool && core_gvfs)
+		core_gvfs = -1;
+}
+
+
+static inline int gvfs_config_load_and_is_set(int mask) {
+	gvfs_load_config_value(0);
+	return gvfs_config_is_set(mask);
+}
+
 
 #endif /* GVFS_H */
