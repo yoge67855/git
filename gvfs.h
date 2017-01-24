c0af3a6e9be4aee7d12e4fe65cc7a1b007d14ec7
Kevin Willford <kewillf@microsoft.com>
2017-01-24 17:54:55 +0100

    gvfs: add the feature that blobs may be missing

--- gvfs.h
+++ gvfs.h
@@ -15,6 +15,7 @@
  */
 #define GVFS_SKIP_SHA_ON_INDEX                      (1 << 0)
 #define GVFS_SPARSE_HASHMAP                         (1 << 1)
+#define GVFS_MISSING_OK                             (1 << 2)
 
 static inline int gvfs_config_is_set(int mask) {
 	return (core_gvfs & mask) == mask;
