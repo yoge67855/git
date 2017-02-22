d7afc82f017534a0351175108bd4768b9a67b5a7
Kevin Willford <kewillf@microsoft.com>
2017-02-22 12:50:43 -0700

    sparse-checkout: update files with a modify/delete conflict
    When using the sparse-checkout feature, the file might not be on disk
    because the skip-worktree bit is on.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -1121,6 +1121,15 @@ static int handle_change_delete(struct merge_options *o,
 				       change, old_path, delete_branch, change_past, path,
 				       change_branch, change_branch, path);
 			}
+			/*
+			 * In a sparse checkout the file may not exist. Sadly,
+			 * the CE_SKIP_WORKTREE flag is not preserved in the
+			 * case of conflicts, therefore we do the next best
+			 * thing: verify that the file exists.
+			 */
+			if (!file_exists(path))
+				ret = update_file(o, 0, o_oid, o_mode,
+						  update_path);
 		} else {
 			if (!old_path) {
 				output(o, 1, _("CONFLICT (%s/delete): %s deleted in %s "
