5dd17a6118deb0382ea97daf09559c279c6227d1
Derrick Stolee <dstolee@microsoft.com>
2017-12-11 10:34:43 -0500

    midx: various cleanups

--- midx.h
+++ midx.h
@@ -5,24 +5,10 @@
 #include "object.h"
 #include "csum-file.h"
 
-#define MIDX_SIGNATURE 0x4d494458 /* "MIDX" */
-#define MIDX_CHUNKID_PACKLOOKUP 0x504c4f4f /* "PLOO" */
-#define MIDX_CHUNKID_PACKNAMES 0x504e414d /* "PNAM" */
-#define MIDX_CHUNKID_OIDFANOUT 0x4f494446 /* "OIDF" */
-#define MIDX_CHUNKID_OIDLOOKUP 0x4f49444c /* "OIDL" */
-#define MIDX_CHUNKID_OBJECTOFFSETS 0x4f4f4646 /* "OOFF" */
-#define MIDX_CHUNKID_LARGEOFFSETS 0x4c4f4646 /* "LOFF" */
-
-#define MIDX_VERSION_GVFS 0x80000001
-#define MIDX_VERSION MIDX_VERSION_GVFS
-
-#define MIDX_HASH_VERSION_SHA1 1
-#define MIDX_HASH_LEN_SHA1 20
-#define MIDX_HASH_VERSION MIDX_HASH_VERSION_SHA1
-#define MIDX_HASH_LEN MIDX_HASH_LEN_SHA1
-
-extern struct object_id *get_midx_head_oid(const char *pack_dir, struct object_id *oid);
-
+extern struct object_id *get_midx_head_oid(const char *pack_dir,
+					   struct object_id *oid);
+extern char *get_midx_head_filename_oid(const char *pack_dir,
+					struct object_id *oid);
 extern int fill_pack_entry_midx(const unsigned char *sha1,
 				struct pack_entry *e);
 
@@ -99,7 +85,7 @@ extern struct midxed_git {
 } *midxed_git;
 
 extern struct midxed_git *get_midxed_git(const char *pack_dir, struct object_id *midx_oid);
-extern int prepare_midxed_git_head(char *pack_dir, int local);
+extern int prepare_midxed_git_objdir(char *obj_dir, int local);
 
 struct pack_midx_details {
 	uint32_t pack_int_id;
@@ -135,11 +121,12 @@ extern int contains_pack(struct midxed_git *m, const char *pack_name);
  *
  * Returns the final name of the MIDX file within pack_dir.
  */
-extern const char *write_midx_file(
-	const char *pack_dir,
-	const char *midx_name,
-	const char **pack_names,          uint32_t nr_packs,
-	struct pack_midx_entry **objects, uint32_t nr_objects);
+extern const char *write_midx_file(const char *pack_dir,
+				   const char *midx_name,
+				   const char **pack_names,
+				   uint32_t nr_packs,
+				   struct pack_midx_entry **objects,
+				   uint32_t nr_objects);
 
 extern int close_midx(struct midxed_git *m);
 extern void close_all_midx(void);
