4fb8ea5064bfebdf857c3d418196b1389143ba8f
Jameson Miller <jamill@microsoft.com>
2018-03-14 16:54:06 -0400

    Enable the creation of transient cache entries
    Signed-off-by: Jameson Miller <jamill@microsoft.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -235,11 +235,11 @@ static int checkout_merged(int pos, const struct checkout *state)
 	if (write_object_file(result_buf.ptr, result_buf.size, blob_type, &oid))
 		die(_("Unable to add merge result for '%s'"), path);
 	free(result_buf.ptr);
-	ce = make_cache_entry_from_index(state->istate, mode, oid.hash, path, 2, 0);
+	ce = make_transient_cache_entry(mode, oid.hash, path, 2);
 	if (!ce)
 		die(_("make_cache_entry failed for path '%s'"), path);
 	status = checkout_entry(ce, state, NULL);
-	cache_entry_free(ce);
+	transient_cache_entry_free(ce);
 	return status;
 }
 
