21feaa39a4f65ccf7b83b4b93c9ddc12ca7c115a
Ben Peart <benpeart@microsoft.com>
2018-04-19 13:46:30 -0400

    merge: Add merge.renames config setting to determine default
    Add the ability to control rename detection for merge via a config setting.
    
    Signed-off-by: Ben Peart <benpeart@microsoft.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -2239,6 +2239,7 @@ static void merge_recursive_config(struct merge_options *o)
 	git_config_get_int("merge.verbosity", &o->verbosity);
 	git_config_get_int("diff.renamelimit", &o->diff_rename_limit);
 	git_config_get_int("merge.renamelimit", &o->merge_rename_limit);
+	git_config_get_bool("merge.renames", &o->detect_rename);
 	git_config(git_xmerge_config, NULL);
 }
 
