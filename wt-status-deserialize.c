d3a3ee671e0fdcfadccd4b7669999f10f79521f7
Jeff Hostetler <jeffhost@microsoft.com>
2018-07-25 12:03:22 -0400

    status: fix rename reporting when using serialization cache
    Fix "git status --deserialize" to correctly report both pathnames
    for renames.  Add a test case to verify.
    
    A change was made upstream that added an additional "rename_status"
    field to the "struct wt_status_change_data" structure.  It is used
    during the various print routines to decide if 2 pathnames need to
    be printed.
    
        5134ccde642ae9ed6a244c92864c26734d100f4c
        wt-status.c: rename rename-related fields in wt_status_change_data
    
    The fix here is to add that field to the status cache data.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- wt-status-deserialize.c
+++ wt-status-deserialize.c
@@ -200,6 +200,7 @@ static int wt_deserialize_v1_changed_items(const struct wt_status *cmd_s,
 		d->worktree_status = ntohl(sd->fixed.worktree_status);
 		d->index_status = ntohl(sd->fixed.index_status);
 		d->stagemask = ntohl(sd->fixed.stagemask);
+		d->rename_status = ntohl(sd->fixed.rename_status);
 		d->rename_score = ntohl(sd->fixed.rename_score);
 		d->mode_head = ntohl(sd->fixed.mode_head);
 		d->mode_index = ntohl(sd->fixed.mode_index);
@@ -218,10 +219,11 @@ static int wt_deserialize_v1_changed_items(const struct wt_status *cmd_s,
 
 		trace_printf_key(
 			&trace_deserialize,
-			"change: %d %d %d %d %o %o %o %d %d %s %s '%s' '%s'",
+			"change: %d %d %d %d %d %o %o %o %d %d %s %s '%s' '%s'",
 			d->worktree_status,
 			d->index_status,
 			d->stagemask,
+			d->rename_status,
 			d->rename_score,
 			d->mode_head,
 			d->mode_index,
