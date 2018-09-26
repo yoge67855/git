edb8e058a297258c7e1bef4877b97caab1b79ee5
Jeff Hostetler <jeffhost@microsoft.com>
2018-09-26 11:21:22 -0400

    gvfs:trace2:data: status deserialization information
    Add trace2 region and data events describing attempts to deserialize
    status data using a status cache.
    
    A category:status, label:deserialize region is pushed around the
    deserialize code.
    
    Deserialization results when reading from a file are:
        category:status, path   = <path>
        category:status, polled = <number_of_attempts>
        category:status, result = "ok" | "reject"
    
    When reading from STDIN are:
        category:status, path   = "STDIN"
        category:status, result = "ok" | "reject"
    
    Status will fallback and run a normal status scan when a "reject"
    is reported (unless "--deserialize-wait=fail").
    
    If "ok" is reported, status was able to use the status cache and
    avoid scanning the workdir.
    
    Additionally, a cmd_mode is emitted for each step: collection,
    deserialization, and serialization.  For example, if deserialization
    is attempted and fails and status falls back to actually computing
    the status, a cmd_mode message containing "deserialize" is issued
    and then a cmd_mode for "collect" is issued.
    
    Also, if deserialization fails, a data message containing the
    rejection reason is emitted.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -151,6 +151,7 @@ static int opt_parse_porcelain(const struct option *opt, const char *arg, int un
 static int do_serialize = 0;
 static char *serialize_path = NULL;
 
+static int reject_implicit = 0;
 static int do_implicit_deserialize = 0;
 static int do_explicit_deserialize = 0;
 static char *deserialize_path = NULL;
@@ -213,7 +214,7 @@ static int opt_parse_deserialize(const struct option *opt, const char *arg, int 
 			deserialize_path = xstrdup(arg);
 		}
 		if (deserialize_path && *deserialize_path
-		    && (access(deserialize_path, R_OK) != 0))
+		    && (wt_status_deserialize_access(deserialize_path, R_OK) != 0))
 			die("cannot find serialization file '%s'",
 			    deserialize_path);
 
@@ -1441,6 +1442,8 @@ static int git_status_config(const char *k, const char *v, void *cb)
 		if (v && *v && access(v, R_OK) == 0) {
 			do_implicit_deserialize = 1;
 			deserialize_path = xstrdup(v);
+		} else {
+			reject_implicit = 1;
 		}
 		return 0;
 	}
@@ -1597,6 +1600,17 @@ int cmd_status(int argc, const char **argv, const char *prefix)
 			   (do_implicit_deserialize || do_explicit_deserialize));
 	if (try_deserialize)
 		goto skip_init;
+	/*
+	 * If we implicitly received a status cache pathname from the config
+	 * and the file does not exist, we silently reject it and do the normal
+	 * status "collect".  Fake up some trace2 messages to reflect this and
+	 * assist post-processors know this case is different.
+	 */
+	if (!do_serialize && reject_implicit) {
+		trace2_cmd_mode("implicit-deserialize");
+		trace2_data_string("status", the_repository, "deserialize/reject",
+				   "status-cache/access");
+	}
 
 	enable_fscache(0);
 	if (status_format != STATUS_FORMAT_PORCELAIN &&
@@ -1640,6 +1654,7 @@ skip_init:
 		if (s.relative_paths)
 			s.prefix = prefix;
 
+		trace2_cmd_mode("deserialize");
 		result = wt_status_deserialize(&s, deserialize_path, dw);
 		if (result == DESERIALIZE_OK)
 			return 0;
@@ -1657,6 +1672,7 @@ skip_init:
 			fd = -1;
 	}
 
+	trace2_cmd_mode("collect");
 	wt_status_collect(&s);
 
 	if (0 <= fd)
@@ -1671,6 +1687,7 @@ skip_init:
 		if (fd_serialize < 0)
 			die_errno(_("could not serialize to '%s'"),
 				  serialize_path);
+		trace2_cmd_mode("serialize");
 		wt_status_serialize_v1(fd_serialize, &s);
 		close(fd_serialize);
 	}
