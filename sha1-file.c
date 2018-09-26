b73ece7e62611b4bed0653797b237a5acc0f6f7d
Jeff Hostetler <jeffhost@microsoft.com>
2018-09-26 12:29:26 -0400

    gvfs:trace2:data: add trace2 tracing around read_object_process
    Add trace2 region around read_object_process to collect
    time spent waiting for missing objects to be dynamically
    fetched.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- sha1-file.c
+++ sha1-file.c
@@ -916,6 +916,8 @@ static int read_object_process(const struct object_id *oid)
 
 	start = getnanotime();
 
+	trace2_region_enter("subprocess", "read_object", the_repository);
+
 	if (!subprocess_map_initialized) {
 		subprocess_map_initialized = 1;
 		hashmap_init(&subprocess_map, (hashmap_cmp_fn)cmd2process_cmp,
@@ -932,13 +934,16 @@ static int read_object_process(const struct object_id *oid)
 		if (subprocess_start(&subprocess_map, &entry->subprocess, cmd,
 				     start_read_object_fn)) {
 			free(entry);
-			return -1;
+			err = -1;
+			goto leave_region;
 		}
 	}
 	process = &entry->subprocess.process;
 
-	if (!(CAP_GET & entry->supported_capabilities))
-		return -1;
+	if (!(CAP_GET & entry->supported_capabilities)) {
+		err = -1;
+		goto leave_region;
+	}
 
 	sigchain_push(SIGPIPE, SIG_IGN);
 
@@ -986,6 +991,10 @@ done:
 	}
 
 	trace_performance_since(start, "read_object_process");
+
+leave_region:
+	trace2_region_leave_printf("subprocess", "read_object", the_repository,
+				   "result %d", err);
 
 	return err;
 }
