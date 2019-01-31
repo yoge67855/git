89c9e224db5e54211e958500e8d679ac0102987b
Jeff Hostetler <jeffhost@microsoft.com>
2019-01-31 08:59:47 -0500

    Revert "gvfs:trace2:data add status serialization/deserialization information"
    This reverts commit f4e9264f145306881f532f305b4e051a0572c033.

--- builtin/commit.c
+++ builtin/commit.c
@@ -145,7 +145,6 @@ static int opt_parse_porcelain(const struct option *opt, const char *arg, int un
 static int do_serialize = 0;
 static char *serialize_path = NULL;
 
-static int reject_implicit = 0;
 static int do_implicit_deserialize = 0;
 static int do_explicit_deserialize = 0;
 static char *deserialize_path = NULL;
@@ -203,7 +202,7 @@ static int opt_parse_deserialize(const struct option *opt, const char *arg, int 
 		if (arg) /* override config or stdin */
 			deserialize_path = xstrdup(arg);
 		if (deserialize_path && *deserialize_path
-		    && (wt_status_deserialize_access(deserialize_path, R_OK) != 0))
+		    && (access(deserialize_path, R_OK) != 0))
 			die("cannot find serialization file '%s'",
 			    deserialize_path);
 
@@ -1397,8 +1396,6 @@ static int git_status_config(const char *k, const char *v, void *cb)
 		if (v && *v && access(v, R_OK) == 0) {
 			do_implicit_deserialize = 1;
 			deserialize_path = xstrdup(v);
-		} else {
-			reject_implicit = 1;
 		}
 		return 0;
 	}
@@ -1555,17 +1552,6 @@ int cmd_status(int argc, const char **argv, const char *prefix)
 			   (do_implicit_deserialize || do_explicit_deserialize));
 	if (try_deserialize)
 		goto skip_init;
-	/*
-	 * If we implicitly received a status cache pathname from the config
-	 * and the file does not exist, we silently reject it and do the normal
-	 * status "collect".  Fake up some trace2 messages to reflect this and
-	 * assist post-processors know this case is different.
-	 */
-	if (!do_serialize && reject_implicit) {
-		trace2_cmd_subverb("implicit-deserialize");
-		trace2_data_string("status", the_repository, "deserialize/reject",
-				   "status-cache/access");
-	}
 
 	enable_fscache(0);
 	if (status_format != STATUS_FORMAT_PORCELAIN &&
@@ -1609,7 +1595,6 @@ skip_init:
 		if (s.relative_paths)
 			s.prefix = prefix;
 
-		trace2_cmd_subverb("deserialize");
 		result = wt_status_deserialize(&s, deserialize_path, dw);
 		if (result == DESERIALIZE_OK)
 			return 0;
@@ -1627,7 +1612,6 @@ skip_init:
 			fd = -1;
 	}
 
-	trace2_cmd_subverb("collect");
 	wt_status_collect(&s);
 
 	if (0 <= fd)
@@ -1642,7 +1626,6 @@ skip_init:
 		if (fd_serialize < 0)
 			die_errno(_("could not serialize to '%s'"),
 				  serialize_path);
-		trace2_cmd_subverb("serialize");
 		wt_status_serialize_v1(fd_serialize, &s);
 		close(fd_serialize);
 	}
