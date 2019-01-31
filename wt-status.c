1d7d271d4b5d28188eb7d3e7828e8c883da126fe
Jeff Hostetler <jeffhost@microsoft.com>
2019-01-31 12:00:18 -0500

    DROPME: trace2:status: use the_repository rather than s->repo
    Temporarily fix wt-status.c in GVFS 2.20.* to use "the_repository"
    rather than "s->repo" which does not yet appear in the this fork.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- wt-status.c
+++ wt-status.c
@@ -749,23 +749,23 @@ static int has_unmerged(struct wt_status *s)
 
 void wt_status_collect(struct wt_status *s)
 {
-	trace2_region_enter("status", "worktrees", s->repo);
+	trace2_region_enter("status", "worktrees", the_repository);
 	wt_status_collect_changes_worktree(s);
-	trace2_region_leave("status", "worktrees", s->repo);
+	trace2_region_leave("status", "worktrees", the_repository);
 
 	if (s->is_initial) {
-		trace2_region_enter("status", "initial", s->repo);
+		trace2_region_enter("status", "initial", the_repository);
 		wt_status_collect_changes_initial(s);
-		trace2_region_leave("status", "initial", s->repo);
+		trace2_region_leave("status", "initial", the_repository);
 	} else {
-		trace2_region_enter("status", "index", s->repo);
+		trace2_region_enter("status", "index", the_repository);
 		wt_status_collect_changes_index(s);
-		trace2_region_leave("status", "index", s->repo);
+		trace2_region_leave("status", "index", the_repository);
 	}
 
-	trace2_region_enter("status", "untracked", s->repo);
+	trace2_region_enter("status", "untracked", the_repository);
 	wt_status_collect_untracked(s);
-	trace2_region_leave("status", "untracked", s->repo);
+	trace2_region_leave("status", "untracked", the_repository);
 
 	wt_status_get_state(&s->state, s->branch && !strcmp(s->branch, "HEAD"));
 	if (s->state.merge_in_progress && !has_unmerged(s))
@@ -2315,12 +2315,12 @@ static void wt_porcelain_v2_print(struct wt_status *s)
 
 void wt_status_print(struct wt_status *s)
 {
-	trace2_data_intmax("status", s->repo, "count/changed", s->change.nr);
-	trace2_data_intmax("status", s->repo, "count/untracked",
+	trace2_data_intmax("status", the_repository, "count/changed", s->change.nr);
+	trace2_data_intmax("status", the_repository, "count/untracked",
 			   s->untracked.nr);
-	trace2_data_intmax("status", s->repo, "count/ignored", s->ignored.nr);
+	trace2_data_intmax("status", the_repository, "count/ignored", s->ignored.nr);
 
-	trace2_region_enter("status", "print", s->repo);
+	trace2_region_enter("status", "print", the_repository);
 
 	switch (s->status_format) {
 	case STATUS_FORMAT_SHORT:
@@ -2344,7 +2344,7 @@ void wt_status_print(struct wt_status *s)
 		break;
 	}
 
-	trace2_region_leave("status", "print", s->repo);
+	trace2_region_leave("status", "print", the_repository);
 }
 
 /**
