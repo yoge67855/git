9dc221dcfb4a983e5ea81e8dd24df125d322d20a
Jeff Hostetler <jeffhost@microsoft.com>
2019-06-03 11:53:08 -0400

    trace2:gvfs:experiment: add prime_cache_tree region
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- cache-tree.c
+++ cache-tree.c
@@ -757,10 +757,14 @@ void prime_cache_tree(struct repository *r,
 		      struct index_state *istate,
 		      struct tree *tree)
 {
+	trace2_region_enter("cache_tree", "prime_cache_tree", r);
+
 	cache_tree_free(&istate->cache_tree);
 	istate->cache_tree = cache_tree();
 	prime_cache_tree_rec(r, istate->cache_tree, tree);
 	istate->cache_changed |= CACHE_TREE_CHANGED;
+
+	trace2_region_leave("cache_tree", "prime_cache_tree", r);
 }
 
 /*
