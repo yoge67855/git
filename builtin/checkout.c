870c0cff2d501af193b531f9a62ea96036c6f528
Jeff Hostetler <jeffhost@microsoft.com>
2019-07-26 08:51:17 -0400

    Trace2:gvfs:experiment: capture more 'tracking' details
    Update tracing around report_tracking() to use 'tracking' category
    rather than 'exp' category.
    
    Add ahead/behind results from stat_tracking_info().
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -929,12 +929,12 @@ static void update_refs_for_switch(const struct checkout_opts *opts,
 	    (new_branch_info->path || (!opts->force_detach && !strcmp(new_branch_info->name, "HEAD")))) {
 		unsigned long nr_unpack_entry_at_start;
 
-		trace2_region_enter("exp", "report_tracking", the_repository);
+		trace2_region_enter("tracking", "report_tracking", the_repository);
 		nr_unpack_entry_at_start = get_nr_unpack_entry();
 		report_tracking(new_branch_info);
-		trace2_data_intmax("exp", NULL, "report_tracking/nr_unpack_entries",
+		trace2_data_intmax("tracking", NULL, "report_tracking/nr_unpack_entries",
 				   (intmax_t)(get_nr_unpack_entry() - nr_unpack_entry_at_start));
-		trace2_region_leave("exp", "report_tracking", the_repository);
+		trace2_region_leave("tracking", "report_tracking", the_repository);
 	}
 }
 
