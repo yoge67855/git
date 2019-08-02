3129d1c24e8368f01de07c11084d86867f1bb238
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-08-02 22:39:48 +0200

    simple-ipc: add a simple test
    This adds a very rudimentary test, just to make sure that the simple
    Inter-Process Communication (where supported) works as intended (and
    maybe also serving as an example how the command listener is supposed to
    be implemented).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ t/t0052-simple-ipc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+test_description='simple command server'
+
+. ./test-lib.sh
+
+test_lazy_prereq SUPPORTS_SIMPLE_IPC '
+	test-tool simple-ipc SUPPORTS_SIMPLE_IPC
+'
+
+test_expect_success SUPPORTS_SIMPLE_IPC 'simple command server' '
+	{ test-tool simple-ipc daemon & } &&
+
+	test-tool simple-ipc send ping >actual &&
+	echo pong >expect &&
+	test_cmp expect actual &&
+
+	test-tool simple-ipc send quit &&
+	test_must_fail test-tool simple-ipc send ping
+'
+
+test_done
+
