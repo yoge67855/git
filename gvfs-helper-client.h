c0ee7e25af813994c80bab02c7a406b4318641c3
Jeff Hostetler <jeffhost@microsoft.com>
2019-08-13 12:12:08 -0400

    gvfs-helper: create tool to fetch objects using the GVFS Protocol
    Create gvfs-helper.  This is a helper tool to use the GVFS Protocol
    REST API to fetch objects and configuration data from a GVFS cache-server
    or Git server.  This tool uses libcurl to send object requests to either
    server.  This tool creates loose objects and/or packfiles.
    
    Create gvfs-helper-client.  This code resides within git proper and
    uses the sub-process API to manage gvfs-helper as a long-running background
    process.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

---
+++ gvfs-helper-client.h
@@ -0,0 +1,61 @@
+#ifndef GVFS_HELPER_CLIENT_H
+#define GVFS_HELPER_CLIENT_H
+
+struct repository;
+struct commit;
+
+enum ghc__created {
+	/*
+	 * The _get_ operation did not create anything.  If doesn't
+	 * matter if `gvfs-helper` had errors or not -- just that
+	 * nothing was created.
+	 */
+	GHC__CREATED__NOTHING  = 0,
+
+	/*
+	 * The _get_operation created one or more packfiles.
+	 */
+	GHC__CREATED__PACKFILE = 1<<1,
+
+	/*
+	 * The _get_ operation created one or more loose objects.
+	 * (Not necessarily the for the individual OID you requested.)
+	 */
+	GHC__CREATED__LOOSE    = 1<<2,
+
+	/*
+	 * The _get_ operation created one or more packfilea *and*
+	 * one or more loose objects.
+	 */
+	GHC__CREATED__PACKFILE_AND_LOOSE = (GHC__CREATED__PACKFILE |
+					    GHC__CREATED__LOOSE),
+};
+
+/*
+ * Ask `gvfs-helper server` to immediately fetch an object.
+ * Wait for the response.
+ *
+ * This may also fetch any queued (non-immediate) objects and
+ * so may create one or more loose objects and/or packfiles.
+ * It is undefined whether the requested OID will be loose or
+ * in a packfile.
+ */
+int ghc__get_immediate(const struct object_id *oid, enum ghc__created *p_ghc);
+
+/*
+ * Queue this OID for a future fetch using `gvfs-helper service`.
+ * It does not wait.
+ *
+ * The GHC layer is free to process this queue in any way it wants,
+ * including individual fetches, bulk fetches, and batching.  And
+ * it may add queued objects to immediate requests.
+ *
+ * Callers should not rely on the queued object being on disk until
+ * the queue has been drained.
+ */
+void ghc__queue_oid(const struct object_id *oid);
+void ghc__queue_oid_array(const struct object_id *oids, int oid_nr);
+
+int ghc__drain_queue(enum ghc__created *p_ghc);
+
+#endif /* GVFS_HELPER_CLIENT_H */
