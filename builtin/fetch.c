7f8481bc1436f3fbbde67adc8e30b22e65ca6756
Derrick Stolee <dstolee@microsoft.com>
2019-12-17 07:43:19 -0500

    fetch: add --no-update-remote-refs
    To allow running "git fetch" in the background, we want to ensure
    that users still see when the remote refs update in their user-
    facing fetch commands. Use a new --no-update-remote-refs option
    to prevent storing these updates to refs/remotes.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -79,6 +79,7 @@ static struct refspec refmap = REFSPEC_INIT_FETCH;
 static struct list_objects_filter_options filter_options;
 static struct string_list server_options = STRING_LIST_INIT_DUP;
 static struct string_list negotiation_tip = STRING_LIST_INIT_NODUP;
+static int update_remote_refs = 1;
 
 static int git_fetch_config(const char *k, const char *v, void *cb)
 {
@@ -200,6 +201,8 @@ static struct option builtin_fetch_options[] = {
 		 N_("run 'gc --auto' after fetching")),
 	OPT_BOOL(0, "show-forced-updates", &fetch_show_forced_updates,
 		 N_("check for forced-updates on all updated branches")),
+	OPT_BOOL(0, "update-remote-refs", &update_remote_refs,
+		 N_("update the refs/remotes/ refspace")),
 	OPT_END()
 };
 
@@ -744,6 +747,9 @@ static int update_local_ref(struct ref *ref,
 	struct branch *current_branch = branch_get(NULL);
 	const char *pretty_ref = prettify_refname(ref->name);
 	int fast_forward = 0;
+
+	if (!update_remote_refs && starts_with(ref->name, "refs/remotes/"))
+		return 0;
 
 	type = oid_object_info(the_repository, &ref->new_oid, NULL);
 	if (type < 0)
