a654a1833576641b4fe39e31975264aef193d56b
Derrick Stolee <dstolee@microsoft.com>
2019-12-17 07:25:40 -0500

    fetch: use gvfs-helper prefetch under config
    The gvfs-helper allows us to download prefetch packs using a simple
    subprocess call. The gvfs-helper-client.h method will automatically
    compute the timestamp if passing 0, and passing NULL for the number
    of downloaded packs is valid.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -27,6 +27,8 @@
 #include "branch.h"
 #include "promisor-remote.h"
 #include "commit-graph.h"
+#include "gvfs.h"
+#include "gvfs-helper-client.h"
 
 #define FORCED_UPDATES_DELAY_WARNING_IN_MS (10 * 1000)
 
@@ -1829,6 +1831,9 @@ int cmd_fetch(int argc, const char **argv, const char *prefix)
 			argv++;
 		}
 	}
+
+	if (core_gvfs & GVFS_PREFETCH_DURING_FETCH)
+		gh_client__prefetch(0, NULL);
 
 	if (remote) {
 		if (filter_options.choice || has_promisor_remote())
