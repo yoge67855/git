64c8e239324c5903339bf70f206829cefbd8d69b
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-12-17 22:05:27 +0100

    simple-ipc: implement a backend based on Unix-sockets
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- simple-ipc.c
+++ simple-ipc.c
@@ -210,4 +210,150 @@ leave_send_command:
 		close(fd);
 	return ret < 0 ? -1 : 0;
 }
+#elif !defined(NO_UNIX_SOCKETS)
+#include "unix-socket.h"
+#include "sigchain.h"
+
+static const char *fsmonitor_listener_path;
+
+int ipc_is_active(const char *path)
+{
+	struct stat st;
+
+	return !lstat(path, &st) && (st.st_mode & S_IFMT) == S_IFSOCK;
+}
+
+static void set_socket_blocking_flag(int fd, int make_nonblocking)
+{
+	int flags;
+
+	flags = fcntl(fd, F_GETFL, NULL);
+
+	if (flags < 0)
+		die(_("fcntl failed"));
+
+	if (make_nonblocking)
+		flags |= O_NONBLOCK;
+	else
+		flags &= ~O_NONBLOCK;
+
+	if (fcntl(fd, F_SETFL, flags) < 0)
+		die(_("fcntl failed"));
+}
+
+static int reply(void *reply_data, const char *response, size_t len)
+{
+	int fd = *(int *)reply_data;
+
+	return write_packetized_from_buf(response, len, fd, 0);
+}
+
+/* in ms */
+#define LISTEN_TIMEOUT 50000
+#define RESPONSE_TIMEOUT 1000
+
+static void unlink_listener_path(void)
+{
+	if (fsmonitor_listener_path)
+		unlink(fsmonitor_listener_path);
+}
+
+int ipc_listen_for_commands(struct ipc_command_listener *listener)
+{
+	int ret = 0, fd;
+
+	fd = unix_stream_listen(listener->path);
+	if (fd < 0)
+		return error_errno(_("could not set up socket for %s"),
+				   listener->path);
+
+	fsmonitor_listener_path = listener->path;
+	atexit(unlink_listener_path);
+
+	trace2_region_enter("simple-ipc", "listen", the_repository);
+	while (1) {
+		struct pollfd pollfd;
+		int result, client_fd;
+		struct strbuf buf = STRBUF_INIT;
+
+		/* Wait for a request */
+		pollfd.fd = fd;
+		pollfd.events = POLLIN;
+		result = poll(&pollfd, 1, LISTEN_TIMEOUT);
+		if (result < 0) {
+			if (errno == EINTR)
+				/*
+				 * This can lead to an overlong keepalive,
+				 * but that is better than a premature exit.
+				 */
+				continue;
+			return error_errno(_("poll() failed"));
+		} else if (result == 0)
+			/* timeout */
+			continue;
+
+		client_fd = accept(fd, NULL, NULL);
+		if (client_fd < 0)
+			/*
+			 * An error here is unlikely -- it probably
+			 * indicates that the connecting process has
+			 * already dropped the connection.
+			 */
+			continue;
+
+		/*
+		 * Our connection to the client is blocking since a client
+		 * can always be killed by SIGINT or similar.
+		 */
+		set_socket_blocking_flag(client_fd, 0);
+
+		strbuf_reset(&buf);
+		result = read_packetized_to_strbuf(client_fd, &buf,
+						   PACKET_READ_NEVER_DIE);
+
+		if (result > 0) {
+			/* ensure string termination */
+			ret = listener->handle_client(listener, buf.buf, reply,
+						      &client_fd);
+			packet_flush_gently(client_fd);
+			if (ret == SIMPLE_IPC_QUIT) {
+				close(client_fd);
+				strbuf_release(&buf);
+				break;
+			}
+		} else {
+			/*
+			 * No command from client.  Probably it's just a
+			 * liveness check or client error.  Just close up.
+			 */
+		}
+		close(client_fd);
+	}
+
+	close(fd);
+	return ret == SIMPLE_IPC_QUIT ? 0 : ret;
+}
+
+int ipc_send_command(const char *path, const char *message,
+		     struct strbuf *answer)
+{
+	int fd = unix_stream_connect(path);
+	int ret = 0;
+
+	sigchain_push(SIGPIPE, SIG_IGN);
+	if (fd < 0 ||
+	    write_packetized_from_buf(message, strlen(message), fd, 1) < 0)
+		ret = -1;
+	else if (answer) {
+		if (read_packetized_to_strbuf(fd, answer,
+					      PACKET_READ_NEVER_DIE) < 0)
+			ret = error_errno(_("could not read packet from '%s'"),
+					  path);
+	}
+
+	if (fd >= 0)
+		close(fd);
+	sigchain_pop(SIGPIPE);
+	return ret;
+}
 #endif
