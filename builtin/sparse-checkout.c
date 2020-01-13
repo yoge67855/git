c861919baced247bb6d46b6f9b47200c5fec7615
Derrick Stolee <dstolee@microsoft.com>
2020-01-13 08:36:12 -0500

    Revert "sparse-checkout: check for dirty status"
    This reverts commit cff4e9138d8df45e3b6199171092ee781cdadaeb.
    
    This is temporary until we fix this behavior upstream. For now,
    we need to allow the sparse-checkout command to run when the
    status is not clean.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- builtin/sparse-checkout.c
+++ builtin/sparse-checkout.c
@@ -12,7 +12,6 @@
 #include "lockfile.h"
 #include "resolve-undo.h"
 #include "unpack-trees.h"
-#include "wt-status.h"
 
 static const char *empty_base = "";
 
@@ -278,10 +277,6 @@ static int sparse_checkout_init(int argc, const char **argv)
 		OPT_END(),
 	};
 
-	repo_read_index(the_repository);
-	require_clean_work_tree(the_repository,
-				N_("initialize sparse-checkout"), NULL, 1, 0);
-
 	argc = parse_options(argc, argv, NULL,
 			     builtin_sparse_checkout_init_options,
 			     builtin_sparse_checkout_init_usage, 0);
@@ -400,10 +395,6 @@ static int sparse_checkout_set(int argc, const char **argv, const char *prefix)
 		OPT_END(),
 	};
 
-	repo_read_index(the_repository);
-	require_clean_work_tree(the_repository,
-				N_("set sparse-checkout patterns"), NULL, 1, 0);
-
 	memset(&pl, 0, sizeof(pl));
 
 	argc = parse_options(argc, argv, prefix,
@@ -462,10 +453,6 @@ static int sparse_checkout_disable(int argc, const char **argv)
 {
 	struct pattern_list pl;
 	struct strbuf match_all = STRBUF_INIT;
-
-	repo_read_index(the_repository);
-	require_clean_work_tree(the_repository,
-				N_("disable sparse-checkout"), NULL, 1, 0);
 
 	memset(&pl, 0, sizeof(pl));
 	hashmap_init(&pl.recursive_hashmap, pl_hashmap_cmp, NULL, 0);
